242cc51a6acc2ed59b8c546574865f8589143f51
Eden <haerim2429@gmail.com>
2021-06-09 17:18:54 +0900

    study print, variable

---
+++ day02/src/day02/PrintTest.java
@@ -0,0 +1,25 @@
+package day02;
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		/*주석: 컴파일러가 읽지 않는다.
+		 *설명글을 달 때 쓰인다.
+		 *범위주석		
+		 *
+		 *개행문자(제어문자)
+		 *\n: 줄바꿈(line)
+		 *\t: 간격을 맞춰서 띄우기(tab)
+		 *
+		 *
+		 */
+		
+		
+		System.out.print("여자"); //줄바꿈x
+		System.out.print("여자\n"); 
+		System.out.println("Eden"); //ln은 line의 약자, 줄바꿈 기능이 있음
+		System.out.println("3월 18일"); //;semi-colon은 끝을 의미함
+		
+	}
+
+}

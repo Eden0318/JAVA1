bd290fc1fd8b81921c839fc77adb1fbf7c169997
Eden <haerim2429@gmail.com>
2021-07-16 21:09:20 +0900

    test

---
+++ exercise/src/exercise/Child.java
@@ -0,0 +1,16 @@
+package exercise;
+
+public class Child extends Parent {
+	public int data3=30;
+	public int data4=40;
+	
+	public void method2() {
+		System.out.println("자식클래스 메서드입니다");
+	}
+	
+//	@Override
+//	public void method1() {
+//		System.out.println("자식클래스에서 재정의 하였습니다");
+//	}
+
+}
---
+++ exercise/src/exercise/Parent.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class Parent {
+	public int data1=10;
+	public int data2=20;
+	
+	public void method1() {
+		System.out.println("부모클래스의 메서드입니다.");
+	}
+}

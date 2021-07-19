30c3d91aff7c6fd596cfb74bb6f3054e3ed5562f
Eden <haerim2429@gmail.com>
2021-07-19 14:18:17 +0900

    study Singleton

---
+++ day26/src/Single/SingleMain.java
@@ -0,0 +1,20 @@
+package Single;
+/*
+ * 패키지명에 .를 사용하면 폴더가 하나 더 만들어진다
+ * a.b.c.d
+ * a폴더 안에 b폴더 안에 c폴더 안에 d
+ */
+
+import java.util.Calendar;
+
+public class SingleMain {
+	public static void main(String[] args) {
+		day26.Single s = day26.Single.getInstance();
+		s.sayHello();
+		Calendar c = Calendar.getInstance();
+		
+		
+		
+		
+	}
+}
---
+++ day26/src/day26/Single.java
@@ -0,0 +1,20 @@
+package day26;
+
+public class Single {
+	
+	//private으로 한 것은 밖에서 new 하지 말라는 것
+	private Single() {
+		
+	}
+	
+	//안에서 객체화
+	private static Single instance = new Single();
+	
+	public static Single getInstance() {
+		return instance;
+	}
+	
+	public void sayHello() {
+		System.out.println("안녕");
+	}
+}

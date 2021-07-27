bcd6d3de667055449249674a14c18afa71c9137e
Eden <haerim2429@gmail.com>
2021-07-27 19:02:46 +0900

    study mvc

---
+++ day30/src/dao/ScoreDAO.java
@@ -0,0 +1,23 @@
+package dao;
+
+public class ScoreDAO {
+	public void join() {
+
+	}
+	
+	public void list() {
+		
+	}
+	
+	public void search() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void delete() {
+		
+	}
+}
